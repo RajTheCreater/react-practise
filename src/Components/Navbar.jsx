8e0d871ab701330ce2825643bdc7ffdb1a89c6c2
Rahul Kushwaha <RajTheCreater@gmail.com>
2026-07-17 22:04:49 +0530

    routers,routes,route,navlink link

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+const Navbar = () => {
+    return (
+        <>
+            <div style={{ background: 'pink', padding: '10px', marginBottom: '10px' }}>
+
+
+                <NavLink to={'/'} style={({ isActive }) => ({
+                    color: isActive ? "white" : "blue",
+                    backgroundColor: isActive ? "green" : "transparent",
+                    padding: "5px 10px",
+                    borderRadius: "5px",
+                    textDecoration: "none",
+                })}>Home</NavLink>
+                <NavLink style={({ isActive }) => ({
+                    color: isActive ? "white" : "blue",
+                    backgroundColor: isActive ? "green" : "transparent",
+                    padding: "5px 10px",
+                    borderRadius: "5px",
+                    textDecoration: "none",
+                })} to={'/about'}>About</NavLink>
+                <NavLink style={({ isActive }) => ({
+                    color: isActive ? "white" : "blue",
+                    backgroundColor: isActive ? "green" : "transparent",
+                    padding: "5px 10px",
+                    borderRadius: "5px",
+                    textDecoration: "none",
+                })} to={'/contact'}>Contact</NavLink>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+       <div>Welcome to Contact Component</div>
+ 
+  )
+}
+
+export default Contact
