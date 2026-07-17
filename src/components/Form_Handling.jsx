3ffa3a4a96b36b4f4aaf1715fdc6deeb3d7dcc9f
Rahul Kushwaha <RajTheCreater@gmail.com>
2026-07-17 20:57:06 +0530

    multiple iinput

--- src/components/Form_Handling.jsx
+++ src/components/Form_Handling.jsx
@@ -2,31 +2,40 @@ import { useState } from "react"
 
 
 const Form_Handling = () => {
-    const [name, setname] = useState('')
-    const [email, setemail] = useState('')
-    const [password, setpassword] = useState('')
-    const onSubmitHandler = (e) => {
+    const [formData, setformData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        phone: ""
+    })
+    const changeHandler = (e) => {
+        const { name, value } = e.target;
+        setformData({ ...formData, [name]: value }) 
+    }
+    const submitHandler = (e) => {
         e.preventDefault();
-        console.log(name, email, password);
+        console.log(formData);
     }
     return (
         <>
-            <form onSubmit={onSubmitHandler}>
+            <form onSubmit={submitHandler}>
                 <div className="mb-3">
-                    <label for="name" className="form-label">Name </label>
-                    <input type="text" onChange={(e) => setname(e.target.value)} value={name} class="form-control" id="name" />
+                    <label for="name" className="form-label">Name </label> {" "}
+                    <input type="text" onChange={changeHandler} name="name" value={formData.name} class="form-control" />
                 </div>
                 <div className="mb-3">
-                    <label for="email" className="form-label">Email</label>
-                    <input type="email" onChange={(e) => setemail(e.target.value)} value={email} class="form-control" id="email" placeholder="name@example.com" />
+                    <label for="email" className="form-label">Email</label> {" "}
+                    <input type="email" onChange={changeHandler} name="email" value={formData.email} class="form-control" />
                 </div>
 
                 <div className="mb-3">
-                    <label for="password" className="form-label">Password</label>
-                    <input type="password" onChange={(e) => setpassword(e.target.value)} value={password} id="password" class="form-control" aria-describedby="passwordHelpBlock" />
-                    <div id="passwordHelpBlock" className="form-text">
-                        Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
-                    </div>
+                    <label for="password" className="form-label">Password</label> {" "}
+                    <input type="password" onChange={changeHandler} name="password" value={formData.password} class="form-control" />
+
+                </div>
+                <div className="mb-3">
+                    <label for="phone" className="form-label">Phone </label> {" "}
+                    <input type="text" onChange={changeHandler} name="phone" value={formData.phone} class="form-control" />
                 </div>
                 <br></br>
                 <div className="mb-3">
