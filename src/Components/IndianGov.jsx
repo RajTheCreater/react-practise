d7331bc469ada5f92b1c083f2c3eeef1c9ce63f3
Rahul Kushwaha <RajTheCreater@gmail.com>
2026-07-20 22:57:36 +0530

    commit

---
+++ src/Components/IndianGov.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import StateGov from './StateGov'
+
+const IndianGov = (money) => {
+    return (
+        <div>
+            <h1>
+                Indian Gov Component
+            </h1>
+            <StateGov money={money} />
+        </div>
+    )
+}
+
+export default IndianGov
