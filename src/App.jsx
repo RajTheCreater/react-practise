d9fd5a6369054157c36f647f1c966e38c02f59c0
Rahul Kushwaha <RajTheCreater@gmail.com>
2026-07-26 21:28:14 +0530

    CustomHook

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 // import Index from './UseTransition/Index'
-import Index from './UseLayoutEffect/Index'
+// import Index from './UseLayoutEffect/Index'
+//import Index from './UseId/Index'
+import Index from './CustomHook/Index'
 
 const App = () => {
   
