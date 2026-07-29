489d7ccdb8ce1415838829019240137414687189
Rahul Kushwaha <RajTheCreater@gmail.com>
2026-07-29 23:33:16 +0530

    toolkit

---
+++ src/app/counterSlice/Index.jsx
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState: 1,
+  reducers: {
+    increaseby1: (state) => state + 1,
+    increaseby10: (state) => state + 10,
+    decreaseby1: (state) => state - 1,
+    decreaseby10: (state) => state - 10,
+    increaseByValue: (state, action) => (state += action.payload),
+  },
+});
+
+export const {
+  increaseby1,
+  increaseby10,
+  decreaseby1,
+  decreaseby10,
+  increaseByValue,
+} = counterSlice.actions;
+
+export default counterSlice.reducer;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,26 @@
+import { useDispatch, useSelector } from "react-redux";
+import {
+    decreaseby1,
+    decreaseby10,
+    increaseby1,
+    increaseby10,
+    increaseByValue
+} from "../app/counterSlice";
+
+const Counter = () => {
+  const count = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>Counter Value = {count}</h1>
+      <button onClick={()=>dispatch(increaseby1())}>increaseby1</button>
+      <button onClick={()=>dispatch(increaseby10())}>increaseby10</button>
+      <button onClick={()=>dispatch(decreaseby1())}>decreaseby1</button>
+      <button onClick={()=>dispatch(decreaseby10())}>decreaseby10</button>
+      <button onClick={()=>dispatch(increaseByValue(1000))}>increasebyValue</button>
+    </div>
+  );
+};
+
+export default Counter;
